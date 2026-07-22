e9d1a43680e296738eff330351f91b6bfd55f689
saraswati7 <saraswatikumar005@gmail.com>
2026-07-22 16:36:02 +0530

    New changes made my tech

--- src/components/MerchantRegistration/AcquiringBank.jsx
+++ src/components/MerchantRegistration/AcquiringBank.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axiosInstance from "../../api/axios";
 import { toast } from "react-toastify";
-
 export default function AcquiringBank({ data, setData, handleNext,
   handleBack }) {
   const [refid, setRefId] = useState("");
